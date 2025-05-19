75fa4718eac8b391a184ec2aa492469c30db7323
fbanier <frbanier@gmail.com>
2025-05-19 15:11:43 +0200

    add getAllUndone method in interface

--- 2. poo/TodoApp-main/src/main/java/org/example/service/TodoService.java
+++ 2. poo/TodoApp-main/src/main/java/org/example/service/TodoService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface TodoService {
     List<Todo> getAll();
+    List<Todo> getAllUndone();
     Todo getById(int id);
     void add(String name, String description);
     void update(int id, String name, String description, boolean done);
