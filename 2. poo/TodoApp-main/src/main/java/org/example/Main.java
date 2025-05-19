fc308143f0570afd91706311deb7115463dd0b39
fbanier <frbanier@gmail.com>
2025-05-19 14:15:15 +0200

    TodoServiceImpl instanciation

--- 2. poo/TodoApp-main/src/main/java/org/example/Main.java
+++ 2. poo/TodoApp-main/src/main/java/org/example/Main.java
@@ -3,11 +3,12 @@ package org.example;
 
 import org.example.ihm.Ihm;
 import org.example.service.TodoService;
+import org.example.service.TodoServiceImpl;
 
 
 public class Main {
     public static void main(String[] args) {
-        TodoService service = null; // remplacer null par une instanciation d'une classe respectant l'interface TodoService
+        TodoService service = new TodoServiceImpl();
         Ihm ihm = new Ihm(service);
         ihm.start();
     }
