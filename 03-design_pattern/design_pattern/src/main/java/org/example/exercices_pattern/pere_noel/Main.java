e43820ba87f572d111bfb6f20b1da6174ff37244
fbanier <frbanier@gmail.com>
2025-05-21 15:18:23 +0200

    Pere noel TP

--- 03-design_pattern/design_pattern/src/main/java/org/example/exercices_pattern/pere_noel/Main.java
+++ 03-design_pattern/design_pattern/src/main/java/org/example/exercices_pattern/pere_noel/Main.java
@@ -6,9 +6,7 @@ import org.example.exercices_pattern.pere_noel.factories.FactoryPlant;
 import org.example.exercices_pattern.pere_noel.factories.FurbyFactory;
 import org.example.exercices_pattern.pere_noel.factories.GameboyFactory;
 import org.example.exercices_pattern.pere_noel.factories.Toy;
-import org.example.exercices_pattern.pere_noel.observers.ElfObserver;
-import org.example.exercices_pattern.pere_noel.observers.GiftList;
-import org.example.exercices_pattern.pere_noel.observers.Observer;
+import org.example.exercices_pattern.pere_noel.observers.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,16 +16,19 @@ public class Main {
         factoryPlant.saveFactory("furby", new FurbyFactory());
         factoryPlant.saveFactory("gameboy", new GameboyFactory());
 
-        Observer elfObserver = new ElfObserver("lutins");
+        Observer furbyObserver = new FurbyObserver("Les lutins qui s'occupent des furby");
+        Observer gameboyObserver = new GameboyObserver("Les lutins qui s'occupent des gameboys");
         Observer giftList = new GiftList("Liste de cadeaux");
 
-        factoryPlant.addObserver(elfObserver);
+        factoryPlant.addObserver(furbyObserver);
+        factoryPlant.addObserver(gameboyObserver);
         factoryPlant.addObserver(giftList);
 
         Toy furbyForTom = factoryPlant.buildToy("furby", "Tom");
         Toy gameboyForLily = factoryPlant.buildToy("gameboy", "Lily");
         Toy furbyForLucy = factoryPlant.buildToy("furby", "Lucy");
         Toy gameboyForNathan = factoryPlant.buildToy("gameboy", "Nathan");
+
 
 
         furbyForLucy = new GiftWrapper(furbyForLucy, "à lignes");
--- 03-design_pattern/design_pattern/src/main/java/org/example/exercices_pattern/pere_noel/observers/FurbyObserver.java
+++ 03-design_pattern/design_pattern/src/main/java/org/example/exercices_pattern/pere_noel/observers/FurbyObserver.java
@@ -1,4 +1,19 @@
 package org.example.exercices_pattern.pere_noel.observers;
 
-public class FurbyObserver {
+import org.example.exercices_pattern.pere_noel.factories.Furby;
+import org.example.exercices_pattern.pere_noel.factories.Gameboy;
+import org.example.exercices_pattern.pere_noel.factories.Toy;
+
+public class FurbyObserver implements Observer{
+    private String name;
+
+    public FurbyObserver(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void notify(Toy toy) {
+        if(toy instanceof Furby) System.out.println(">> Pour " + name + ": " + toy.getToy());
+    }
+
 }
