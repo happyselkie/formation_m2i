8252de1e530d56f7336b9af2047553e2d64ffe05
fbanier <frbanier@gmail.com>
2025-06-13 16:32:40 +0200

    ex06 API with response

--- 07-j2e/exercice/ex06/src/main/java/org/example/ex06/CarResource.java
+++ 07-j2e/exercice/ex06/src/main/java/org/example/ex06/CarResource.java
@@ -9,6 +9,7 @@ import org.example.ex06.service.CarService;
 import java.util.List;
 
 @Path("/cars")
+@Produces(MediaType.APPLICATION_JSON)
 public class CarResource {
 
     CarService carService;
@@ -19,20 +20,17 @@ public class CarResource {
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Car> getCars() {
         return carService.getCars();
     }
 
     @GET
     @Path("/details/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Car getCar(@PathParam("id") int id) {
         return carService.getCar(id);
     }
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Car createCar(Car car) {
         return carService.save(car.getBrand(),car.getYear(), car.getColor());
@@ -40,21 +38,18 @@ public class CarResource {
 
     @POST
     @Path("/create-with-service")
-    @Produces(MediaType.APPLICATION_JSON)
     public Car createCarWithService(){
         return carService.save("Peugeot", 2024, "blue");
     }
 
     @POST
     @Path("/create-with-params")
-    @Produces(MediaType.APPLICATION_JSON)
     public Car createCarWithParams(@QueryParam("brand") String brand, @QueryParam("year") int year, @QueryParam("color") String color) {
         return carService.save(brand, year, color);
     }
 
     @PUT
     @Path("/update/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Car updateCarWithParams(@PathParam("id") int id, @QueryParam("brand") String brand, @QueryParam("year") int year, @QueryParam("color") String color) {
         return carService.update(id, brand, year, color);
     }
